ef23b518f4bc1829b594ee8140531448ba0f9f05
Mario Pizzinini <hello@crafted.systems>
2019-08-27 11:09:07 +0200

    set limits for memory usage and execution time

--- server/checkit.php
+++ server/checkit.php
@@ -1,5 +1,9 @@
 <?php
 
+// Set limits
+ini_set('max_execution_time', 120);
+ini_set('memory_limit', '256M');
+
 // Turn it all on.
 error_reporting(E_ALL | E_STRICT);
 @ini_set('display_errors', '1');
