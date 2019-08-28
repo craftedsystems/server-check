4fb7708b96bf2f96964bb2c3955b461676ecbdc7
Mario Pizzinini <hello@crafted.systems>
2019-08-28 10:31:39 +0200

    remove limits as server does not respect them

--- server/checkit.php
+++ server/checkit.php
@@ -1,9 +1,5 @@
 <?php
 
-// Set limits
-ini_set('max_execution_time', 120);
-ini_set('memory_limit', '256M');
-
 // Turn it all on.
 error_reporting(E_ALL | E_STRICT);
 @ini_set('display_errors', '1');
